d6999de8813884eb0a05b09198b2009166027a2c
antonzimmermann05-dotcom <anton.zimmermann05@gmail.com>
2025-12-01 20:29:09 +0100

    Add files via upload

---
+++ sdk/constants/mcp-server.ts
@@ -0,0 +1,15 @@
+export type McpServerId = keyof typeof MCP_SERVERS;
+
+// { "mcp-id": { id: string, name: string, url: string } }
+export const MCP_SERVERS = {
+  "686de4616fd1cae1afbb55b9": {
+    "id": "686de4616fd1cae1afbb55b9",
+    "name": "Notion",
+    "url": "https://backend.composio.dev/v3/mcp/4c5f76f7-bbb8-4a8a-a478-ab5a2d7b0fa2/mcp?user_id=692db36a6e6bf95336328bd6"
+  },
+  "68f0a290f81ae7b79782adc9": {
+    "id": "68f0a290f81ae7b79782adc9",
+    "name": "Firecrawl",
+    "url": "https://backend.composio.dev/v3/mcp/6f8c6c8b-f19d-4152-b3ac-3c62f7ff4540/mcp?user_id=692db36a6e6bf95336328bd6"
+  }
+};
